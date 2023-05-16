9537c2137534a8c01df1b611455e7ea3849b2291
praprf <fil18mot@gmail.com>
2023-05-16 18:22:03 +0300

    added eternalMovement of comoponents

---
+++ ClickerBacker/Assets/Scripts/ComponentsButton.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComponentsButton : MonoBehaviour
+{
+    [SerializeField] private Button _buttonRight;
+    [SerializeField] private Button _buttonLeft;
+    [SerializeField] private GameObject[] _images;
+    private int _index;
+    private void Start()
+    {
+       for(int i = 1; i<_images.Length;i++)
+        {
+            _images[i].SetActive(false);
+        }
+        _images[0].SetActive(true);
+    }
+    private void Awake()
+    {
+        _buttonRight.onClick.AddListener(EternalMoveRight);
+        _buttonLeft.onClick.AddListener(EternalMoveLeft);
+    }
+
+    public void EternalMoveRight()
+    {
+        if (_index == _images.Length - 1)
+        {
+            _index = 0;
+            _images[_index].SetActive(true);
+            _images[_images.Length - 1].SetActive(false);
+        }
+
+        else
+        {
+            _index++;
+            _images[_index].SetActive(true);
+            _images[_index - 1].SetActive(false);
+        }
+    }
+    public void EternalMoveLeft()
+    {
+        if (_index == 0)
+        {
+            _index= _images.Length - 1;
+            _images[_index].SetActive(true);
+            _images[0].SetActive(false);
+        }
+
+        else
+        {
+            _index--;
+            _images[_index].SetActive(true);
+            _images[_index + 1].SetActive(false);
+        }
+    }
+}
+
