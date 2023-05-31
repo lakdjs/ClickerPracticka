437987c1c66da18e1cee9998191bf3d918487ab5
LeiGGaRFeeD <ridefoot@gmail.com>
2023-06-01 01:03:17 +0300

    Fixed git

---
+++ Assets/Scripts/BuyingImages.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyingImages : MonoBehaviour
+{
+    [SerializeField] private Image _imageForBuying;
+    [SerializeField] private Image _imageLocked;
+    [SerializeField] private Button _buttonForBuying;
+    private void Awake()
+    {
+
+        PlayerPrefs.SetInt(_imageForBuying.name, 0);
+        _buttonForBuying.onClick.AddListener(BuyingImage);
+        Debug.Log(_imageForBuying.name);
+    }
+    private void Start()
+    {
+         
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.I))
+        {
+            Debug.Log(PlayerPrefs.GetInt(_imageForBuying.name));
+        }
+        if (PlayerPrefs.GetInt(_imageForBuying.name)==0)
+        {
+            _imageForBuying.enabled = false;
+            _imageLocked.enabled = true;
+        }
+        else if(PlayerPrefs.GetInt(_imageForBuying.name)>0)
+        {
+            _imageForBuying.enabled = true;
+            _imageLocked.enabled = false;
+        }
+    }
+    public void BuyingImage()
+    {
+        Debug.Log("Bought");
+        PlayerPrefs.SetInt(_imageForBuying.name,1);
+    }
+}
