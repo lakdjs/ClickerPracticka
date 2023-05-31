ed6f4f939afd7d99a1b13840d6050f0c846ff4fd
LeiGGaRFeeD <ridefoot@gmail.com>
2023-05-31 16:41:23 +0300

    Scene2 completed
    Completed Scene 2 for level
    Need to do tutorial in future

---
+++ ClickerBacker/Assets/Scripts/Plot/SceneSelectorForPlot.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+//Script For plot buttons
+public class SceneSelectorForPlot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+   public void goToSceneOne()
+    {
+        Debug.Log("Going to plot scene one");
+        SceneManager.LoadScene("Scene1");
+    }
+    public void goToSceneTwo()
+    {
+        Debug.Log("Going to plot scene two");
+        SceneManager.LoadScene("Scene2");
+    }
+    public void goToSceneThree()
+    {
+        Debug.Log("Going to plot scene three");
+        SceneManager.LoadScene("Scene3");
+    }
+    public void goToSceneFour()
+    {
+        Debug.Log("Going to plot scene four");
+        SceneManager.LoadScene("Scene4");
+    }
+    public void goToSceneFive()
+    {
+        Debug.Log("Going to plot scene five");
+        SceneManager.LoadScene("Scene5");
+    }
+    public void goToSceneSix()
+    {
+        Debug.Log("Going to plot scene six");
+        SceneManager.LoadScene("Scene6");
+    }
+    public void goToSceneSeven()
+    {
+        Debug.Log("Going to plot scene seven");
+        SceneManager.LoadScene("Scene7");
+    }
+    public void goToSceneEight()
+    {
+        Debug.Log("Going to plot scene eight");
+        SceneManager.LoadScene("Scene8");
+    }
+    public void goToSceneNine()
+    {
+        Debug.Log("Going to plot scene nine");
+        SceneManager.LoadScene("Scene9");
+    }
+    public void goToSceneTen()
+    {
+        Debug.Log("Going to plot scene ten");
+        SceneManager.LoadScene("Scene10");
+    }
+    public void goToSceneEleven()
+    {
+        Debug.Log("Going to plot scene eleven");
+        SceneManager.LoadScene("Scene11");
+    }
+    public void goToSceneTwelve()
+    {
+        Debug.Log("Going to plot scene twelve");
+        SceneManager.LoadScene("Scene12");
+    }
+    public void goToSceneThirteen()
+    {
+        Debug.Log("Going to plot scene thirteen");
+        SceneManager.LoadScene("Scene13");
+    }
+    public void goToSceneFourteen()
+    {
+        Debug.Log("Going to plot scene fourteen");
+        SceneManager.LoadScene("Scene14");
+    }
+    public void goToSceneFifthteen()
+    {
+        Debug.Log("Going to plot scene fifthteen");
+        SceneManager.LoadScene("Scene15");
+    }
+    public void goToSceneSixteen()
+    {
+        Debug.Log("Going to plot scene sixteen");
+        SceneManager.LoadScene("Scene16");
+    }
+    public void goToSceneSeventeen()
+    {
+        Debug.Log("Going to plot scene seventeen");
+        SceneManager.LoadScene("Scene17");
+    }
+    public void goToSceneEightteen()
+    {
+        Debug.Log("Going to plot scene eighteen");
+        SceneManager.LoadScene("Scene18");
+    }
+    public void goToSceneNineteen()
+    {
+        Debug.Log("Going to plot scene nineteen");
+        SceneManager.LoadScene("Scene19");
+    }
+    public void goToSceneTwenty()
+    {
+        Debug.Log("Going to plot scene twenty");
+        SceneManager.LoadScene("Scene20");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
