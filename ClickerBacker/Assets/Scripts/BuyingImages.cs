34f99efd555fec55f0f16b19fa339de91b6129da
praprf <fil18mot@gmail.com>
2023-05-31 19:02:27 +0300

    realized shop, but smth doesnt work

---
+++ ClickerBacker/Assets/Scripts/BuyingImages.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyingImages : MonoBehaviour
+{
+    [SerializeField] private Image _imageForBuying;
+    [SerializeField] private Image _imageLocked;
+    [SerializeField] private Button _buttonForBuying;
+    private void Start()
+    {
+        _buttonForBuying.onClick.AddListener(BuyingImage);
+    }
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.I))
+        {
+            Debug.Log(PlayerPrefs.GetInt(_imageForBuying.name));
+        }
+        if (PlayerPrefs.GetInt(_imageForBuying.name)==0)
+        {
+            _imageForBuying.enabled = false;
+            _imageLocked.enabled = true;
+        }
+        else if(PlayerPrefs.GetInt(_imageForBuying.name)>0)
+        {
+            _imageForBuying.enabled = true;
+            _imageLocked.enabled = false;
+        }
+    }
+    public void BuyingImage()
+    {
+        Debug.Log("Bought");
+        PlayerPrefs.SetInt(_imageForBuying.name,1);
+    }
+}
