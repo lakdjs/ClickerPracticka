6cd7c5fe3c404e64d9a3399a4fb06e936cee2106
praprf <fil18mot@gmail.com>
2023-06-01 01:49:19 +0300

    Added surpriseBox

---
+++ Assets/Scripts/StopTimer.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopTimer : MonoBehaviour
+{
+    [SerializeField] private TimerScript _timerScript;
+
+}
